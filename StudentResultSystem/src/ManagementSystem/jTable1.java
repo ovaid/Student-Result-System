9f030cbf444ba7333699b9d8609ed38658e6d924
Rameez Raja <ovaidmushtaq.ovi@gmail.com>
2022-10-29 16:02:31 +0530

    first commit

---
+++ StudentResultSystem/src/ManagementSystem/jTable1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ManagementSystem;
+
+import javax.swing.table.TableModel;
+
+/**
+ *
+ * @author asus
+ */
+class jTable1 {
+
+    static void setModel(TableModel resultSetToTableModel) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
